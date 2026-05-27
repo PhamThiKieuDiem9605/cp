5f6bc7c9d89c87168a3ea7d0494ea9ae5f880403
Pham Thi Kieu Diem <laptrinh0906@gmail.com>
2026-05-27 15:57:12 +0700

    pytest counting

---
+++ auto_test_counting.py
@@ -0,0 +1,19 @@
+from counting import max_freq
+
+def test_case_1():
+    nums = [1,1,1,2,3]
+    result = max_freq(nums)
+    assert result == 1
+    print("Test case 1 passed!")
+
+def test_case_2():
+    nums = [1,2,2,2,3]
+    result = max_freq(nums)
+    assert result == 1
+    print("Test case 2 passed!")
+
+def test_case_3():
+    nums = [3,1,1,2,3]
+    result = max_freq(nums)
+    assert result == 2
+    print("Test case 3 passed!")
